8e8a15cb4f7ace9aaa26c6c7a2676c6a669dcb57
saumya19098 <saumya19098@iiitd.ac.in>
2019-11-30 16:38:58 +0530

    Register pages are made.

--- app/src/main/java/com/example/oopd_project/AdminLogin.java
+++ app/src/main/java/com/example/oopd_project/AdminLogin.java
@@ -2,13 +2,27 @@ package com.example.oopd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminLogin extends AppCompatActivity {
+    Button Register_Admin_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        Register_Admin_button=(Button)findViewById(R.id.Admin_register);
+        Register_Admin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Register_Admin.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/oopd_project/Main2Activity.java
+++ app/src/main/java/com/example/oopd_project/Main2Activity.java
@@ -2,13 +2,25 @@ package com.example.oopd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+    Button User_Register_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        User_Register_Button=(Button)findViewById(R.id.User_Register);
+        User_Register_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Register_User.class);
+                startActivity(intent);
+            }
+        });
     }
 }
