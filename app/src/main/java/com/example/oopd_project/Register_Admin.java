e1275ef2e061c86793c77056e4c3f87a8d039fb3
saumya19098 <saumya19098@iiitd.ac.in>
2019-11-30 19:16:25 +0530

    commit 3

---
+++ app/src/main/java/com/example/oopd_project/Register_Admin.java
@@ -0,0 +1,14 @@
+package com.example.oopd_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__admin);
+    }
+}
