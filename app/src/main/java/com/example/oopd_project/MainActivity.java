ec4f721a85d4202fa25ade1269b4eeecbe44e7b4
saumya19098 <saumya19098@iiitd.ac.in>
2019-11-30 14:35:02 +0530

    1st Page is added

--- app/src/main/java/com/example/oopd_project/MainActivity.java
+++ app/src/main/java/com/example/oopd_project/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.oopd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button User_Button;
+    Button Admin_Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Admin_Button=(Button)findViewById(R.id.button_admin);
+        User_Button=(Button)findViewById(R.id.button_user);
+        Admin_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+        User_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
